a17fedede3950e54ec458844343f2b58d90557b4
BhavarajuSriVaishnavi <58408302+BhavarajuSriVaishnavi@users.noreply.github.com>
2019-12-07 12:29:14 +0530

    Add files via upload

---
+++ file1.c
@@ -0,0 +1,11 @@
+control structure types
+A.conditional control structures
+1)for loop
+2)while loop
+3)do-while loop
+B.unconditional control structures
+4)break
+5)continue
+c.unconditional uncontrol structure
+6)goto
+
---
+++ file2.c
@@ -0,0 +1,10 @@
+for loop:	
+syn:
+for(initialization;condition;inc/dec)	
+   statement;   
+for(initialization;condition;inc/dec)
+{
+statement-1;
+statement-2;
+........statement-x;	
+}   
